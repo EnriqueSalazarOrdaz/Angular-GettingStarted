da9e79fa95081ff85fef11a7cb65f059d9276525
Enrique Salazar Ordaz <Enrique.Salazar@compac.com.mx>
2019-06-11 12:51:40 -0500

    creacion del primer componente y agregado de boostrap y font awesome al main styles.css

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:"pm-root",
+  //styleUrls:[],
+  template:`
+    <div><h1>{{pageTitle}}</h1>
+      <div>fist component</div>
+    </div>
+  `
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle:string='Acme Product Management';
 }
