8ac1c1d92f60c721aca1fb93fdf5b835d5f89a29
Enrique Salazar <EnriqueSa7@gmail.com>
2019-06-13 21:37:31 -0500

    add routing to module and change template to app.component.ts

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector:"pm-root",
-  //styleUrls:[],
-  template:`
-    <div><h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
+  selector: "pm-root",
+  styleUrls: ['./app.component.css'],
+  template: `
+    <nav class='navbar navbar-expand navbar-light bg-light'>
+        <a class='navbar-brand'>{{pageTitle}}</a>
+        <ul class='nav nav-pills'>
+          <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/welcome']">Home</a></li>
+          <li><a class='nav-link' routerLinkActive='active' [routerLink]="['/products']">Product List</a></li>
+        </ul>
+    </nav>
+    <div class='container'>
+      <router-outlet></router-outlet>
     </div>
   `
 })
 
 export class AppComponent {
-  pageTitle:string='Acme Product Management';
+  pageTitle: string = 'Acme Product Management';
 }
